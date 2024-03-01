ef5df38d5bd660eec18b48aa8bc9a6dec7f16bcd
Adam Nicholas Werbik <69626130+adamnwerbik@users.noreply.github.com>
2024-03-01 18:00:37 +1100

    builds

--- app/(pages)/myhabits/[id]/components/HabitActivityCalendar.tsx
+++ app/(pages)/myhabits/[id]/components/HabitActivityCalendar.tsx
@@ -10,7 +10,7 @@ const HabitActivityCalendar = (props: any) => {
         hideColorLegend={true}
         hideTotalCount={false}
         showWeekdayLabels={true}
-        data={dates}
+        data={props.dates}
         theme={{
           light: ["hsl(0, 0%, 92%)", "rgb(53,137,96)"],
           dark: ["#333", "rgb(214, 16, 174)"],
