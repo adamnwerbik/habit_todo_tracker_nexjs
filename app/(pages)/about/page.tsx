5de89bffeab6fa3a0b30119b406211dc69899326
Adam Nicholas Werbik <69626130+adamnwerbik@users.noreply.github.com>
2024-03-05 20:21:48 +1100

    del

--- app/(pages)/about/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>ABOUT</div>;
-};
-
-export default page;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthButton from "@/components/AuthButton";
-
-export default async function Index() {
-  return (
-    <div className="max-w-80 sm:max-w-sm md:max-w-md lg:max-w-lg">
-      <h1>Hi there!</h1>
-    </div>
-  );
-}
